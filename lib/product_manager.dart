bdaf526a9475141523ca15737a1cbd66ccbf748a
iamsss <saurav9760@gmail.com>
2019-01-19 11:33:58 +0530

    Changing State with stateless widget

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,40 +1,44 @@
 import 'package:flutter/material.dart';
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager({this.startingProduct = 'Sweets Tester'}); // Now Product MAnager has default value
+  ProductManager(
+      {this.startingProduct =
+          'Sweets Tester'}); // Now Product MAnager has default value
 
   @override
-    State<StatefulWidget> createState() {
-      return _ProductManagerState();
-    }
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  
   List<String> _products = [];
 
   @override
-    void initState() {
-      super.initState();
-      _products.add(widget.startingProduct);
-    }
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+  }
+
+  void _addProducts(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
-    Widget build(BuildContext context) {
-      return Column(children: [Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton( 
-                  color: Theme.of(context).primaryColor,
-                  onPressed: () {
-                    setState(() {        
-                    _products.add('Product 2'); 
-                                        });
-                  },
-                  child: Text('Add Products'),
-                ),
-              ),
-              Products(_products)
-              ],);
-    }
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProducts),
+        ),
+        Products(_products)
+      ],
+    );
+  }
 }
